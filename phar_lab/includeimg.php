b21d08174a9ea6a14210d650f57242b0b8fe4d83
diego-tella <diegotellaroli05@gmail.com>
2024-06-01 17:58:07 -0300

    Exploit

--- phar_lab/includeimg.php
+++ phar_lab/includeimg.php
@@ -1,3 +1,16 @@
 <?php
 
+class AnyClass {
+	public $data = null;
+	public function __construct($data) {
+		$this->data = $data;
+	}
+	
+	function __destruct() { //to execute command when we quit the class! Let's use this to create a backup of the files!
+		system($this->data);
+	}
+}
+
 echo '<img src="data:image/png;base64,'.base64_encode(file_get_contents($_GET['file'])).'"/>';
+
+$myClass = new AnyClass("/tmp/backupImg.sh");
